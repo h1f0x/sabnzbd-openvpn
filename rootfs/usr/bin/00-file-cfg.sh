e13f5566909d2b0d6ec4e343807a3eb79bfbfc4c
h1f0x <9852011+h1f0x@users.noreply.github.com>
2019-04-01 19:05:31 +0200

    piiep.

--- rootfs/usr/bin/00-file-cfg.sh
+++ rootfs/usr/bin/00-file-cfg.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env bash
 
-# OpenVPN
+mkdir -p /output/incomplete
+mkdir -p /output/complete
+mkdir -p /config/sabnzbd/watch
+mkdir -p /config/sabnzbd/admin
+mkdir -p /config/sabnzbd/nzb_backup
+mkdir -p /config/sabnzbd/email
+mkdir -p /config/sabnzbd/scripts
 mkdir -p /config/vpn
+
+# OpenVPN
 if [ ! -f /config/vpn/client.conf ]; then
     cp -r /defaults/config/vpn/client.conf /config/vpn/client.conf
 fi
@@ -11,20 +19,14 @@ if [ ! -f /config/vpn/vpn.auth ]; then
 fi
 
 # sabnzbd
-mkdir -p /config/sabnzbd
 if [ ! -f /config/sabnzbd/config.ini ]; then
     cp -r /defaults/config/sabnzbd/config.ini /config/sabnzbd/config.ini
 fi
 
-mkdir -p /output/incomplete
-mkdir -p /output/complete
-mkdir -p /config/sabnzbd/watch
-mkdir -p /config/sabnzbd/admin
-mkdir -p /config/sabnzbd/nzb_backup
-mkdir -p /config/sabnzbd/email
-mkdir -p /config/sabnzbd/scripts
 
 chown -R sabnzbd:sabnzbd /output
 chown -R sabnzbd:sabnzbd /config/sabnzbd
 
 chmod -R 755 /config/sabnzbd
+
+systemctl restart sabnzbd.service
